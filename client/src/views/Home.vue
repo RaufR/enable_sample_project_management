96e0f74069c9599509c637bd82576f8d9ba393b7
Raufur Rahman <rahmanrau@BISTUDIO.LOC>
2018-09-19 13:13:28 +0200

    Initial commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+     <div class="container-center">
+       <h2>Welcome!</h2>
+       <div>Enter your email address to start free trial </div>
+       <div v-if="error" class="el-alert--error">{{ error }}</div>
+     </div>
+      <el-form>
+        <el-form
+      </el-form>
+
+
+    </el-main>
+  </el-container>
+
+
+</template>
+
+<script>
+
+</script>
